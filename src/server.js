8c7690e013ef81ce014957bc3ce0fb74706b81a1
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-02-04 01:56:02 +0200

    .

--- src/server.js
+++ src/server.js
@@ -62,6 +62,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/' });
 
-const httpServer = app.listen({ port: 4000 }, () => {
+const PORT = process.env.port || 4000;
+const httpServer = app.listen(PORT, () => {
   console.log(`🚀  Server ready at port ${httpServer.address().port}`);
 })
